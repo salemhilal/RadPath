15aa5280b3ca4cf3416ed92acbc4e5bb5e4124f6
Salem <salemhilal@gmail.com>
2015-04-21 02:03:35 -0400

    Open case = read

--- js/app.js
+++ js/app.js
@@ -198,6 +198,7 @@ app.controller('RadPathController', function($scope) {
 	// Shows a followup case in detail
 	$scope.showCaseDetail = function(patient) {
 		$scope.detailCase = patient;
+		patient.read = true;
 		$scope.detail = true;
 	};
 
